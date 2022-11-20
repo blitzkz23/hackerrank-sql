ad8e9e84e199a7546b20518e2e09ac2c0858c687
blitzkz23 <blitzaldykazu@gmail.com>
2022-11-21 05:10:40 +0700

    add ollivander's inventory problem

--- basic/basic_join.sql
+++ basic/basic_join.sql
@@ -22,12 +22,25 @@ ORDER BY grades.grade DESC, students.name ASC
 
 # Top Competitors [https://www.hackerrank.com/challenges/full-score/problem?isFullScreen=true]
 SELECT h.hacker_id, h.name
-FROM submissions s
-INNER JOIN hackers h ON s.hacker_id = h.hacker_id
-INNER JOIN challenges c ON s.challenge_id = c.challenge_id
-INNER JOIN difficulty d ON c.difficulty_level = d.difficulty_level
+FROM submissions AS s
+INNER JOIN hackers AS h ON s.hacker_id = h.hacker_id
+INNER JOIN challenges AS c ON s.challenge_id = c.challenge_id
+INNER JOIN difficulty AS d ON c.difficulty_level = d.difficulty_level
 WHERE s.score = d.score 
 AND c.difficulty_level = d.difficulty_level
 GROUP BY h.hacker_id, h.name
 HAVING COUNT(s.hacker_id) > 1
 ORDER BY COUNT(s.hacker_id) DESC, s.hacker_id ASC;
+
+# Ollivanders Inventory [https://www.hackerrank.com/challenges/harry-potter-and-wands/problem?isFullScreen=true]
+SELECT w.id, wp.age, w.coins_needed, w.power
+FROM wands AS w
+INNER JOIN wands_property AS wp ON w.code = wp.code
+WHERE wp.is_evil = 0 AND w.coins_needed = 
+    (
+        SELECT MIN(coins_needed)
+        FROM wands AS w2
+        INNER JOIN wands_property AS wp2 ON w2.code = wp2.code
+        WHERE w2.power = w.power AND wp2.age = wp.age
+    )
+ORDER BY w.power DESC, wp.age DESC
